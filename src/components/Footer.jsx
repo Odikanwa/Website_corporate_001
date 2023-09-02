423422b2db49b47ccc4665bc48c2cc266d35cd19
Michael Odikanwa <odikanwamichael@gmail.com>
2023-09-03 00:52:31 +0100

    modified footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import { styles } from "../styles";
 
 const Footer = () => {
   return (
-    <div className="h-auto lg:h-[55vh] w-[100vw] flex flex-col content-center justify-center place-items-center text-white bg-[#2257bf] px-[10vw] py-[3vh] text-[14px]">
+    <div className="h-auto lg:h-[65vh] w-[100vw] flex flex-col content-center justify-center place-items-center text-white bg-[#2257bf] px-[10vw] py-[3vh] text-[14px]">
         <div className="h-auto w-[100%] flex flex-row flex-wrap pt-[5%]">
             <div className="flex flex-col w-[50%] lg:w-[33%]">
                 <img src={logo} alt="logo" color="red" width={125} height={95} className=" object-contain mt-0 pt-0" />
@@ -19,8 +19,10 @@ const Footer = () => {
                 <h1 className="font-bold text-lg py-[10px]">Services</h1>
                 <Link to="/microsoft365" className="py-[5px] cursor-pointer">Microsoft 365</Link>
                 <Link to="/domainhosting" className="py-[5px] cursor-pointer">Domain Hosting</Link>
+                <Link to="/dynamics365" className="py-[5px] cursor-pointer">Dynamics 365</Link>
                 <Link to="/websitedev" className="py-[5px] cursor-pointer">Website templates</Link>
                 <Link to="/surveillance" className="py-[5px] cursor-pointer">Surveillance systems</Link>
+                <Link to="/managedwifi" className="py-[5px] cursor-pointer">Managed Wifi</Link>
                 <Link className="py-[5px] cursor-pointer">Hardware Marketplace</Link>
             </div>
             <div className="w-[50%] lg:w-[33%]">
